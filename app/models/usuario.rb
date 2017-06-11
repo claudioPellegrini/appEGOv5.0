5f9a97d51d589df33859f429b0f758e2a6b8b980
¨Eugenia¨ <mauge58@gmail.com>
2017-06-11 17:14:26 -0300

    otrasValidacionesUsuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,9 @@ class Usuario < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :ci, presence: {message: "^Debe ingresar su cedula de identidad"}
+  validates :nombres, presence: {message: "^Debe ingresar sus nombres"}
+  validates :apellidos, presence: {message: "^Debe ingresar sus apellidos"}
+  validates :rol, presence: {message: "^Debe seleccionar un rol"}
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :franjas
   resources :productos
   resources :tipos
+  resources :usuarios
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
