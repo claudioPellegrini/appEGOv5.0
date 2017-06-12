9806651377e2ff5425b19c607191ededfa2bb886
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-06-12 12:10:10 -0300

    divs ocultos segun rol

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+  	if usuario_signed_in?
+  		@nombre = current_usuario.nombres
+  	end
+  	if current_usuario.rol == "ADMINISTRADOR"
+  		@div_admin = true
+  	end
+  	if current_usuario.rol == "USUARIO"
+  		@div_usuario = true
+  	end
   end
+
+
 end
