f0be325f91c3d533cdc648d043a0799998ac4f23
¨Eugenia¨ <mauge58@gmail.com>
2017-07-10 19:34:40 -0300

    fallback:none

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
   class ApplicationController < ActionController::Base
   
-  acts_as_token_authentication_handler_for Cuentum, fallback: :devise
+  acts_as_token_authentication_handler_for Cuentum, fallback: :none
   
   
   protect_from_forgery unless: -> { request.format.json? } 
