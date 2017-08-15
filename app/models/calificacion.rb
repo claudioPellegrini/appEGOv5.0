3bd453827b27c43e27a53d8d3877a540effe5386
¨Eugenia¨ <mauge58@gmail.com>
2017-08-15 19:56:48 -0300

    ControloCalificacion

--- app/models/calificacion.rb
+++ app/models/calificacion.rb
@@ -1,6 +1,6 @@
 class Calificacion < ApplicationRecord
 	belongs_to :compra
 	
-	
-
+	validates :compra_id, presence: {message: "^Debe seleccionar una compra"}
+	validates :valor, presence: {message: "^Debe ingresar un valor"}
 end
