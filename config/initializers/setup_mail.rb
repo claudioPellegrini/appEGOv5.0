a29d28ac061887d55f74bb09a0d916e0aebd110f
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-08-17 14:53:20 -0300

    prueba correo gmx

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -19,7 +19,7 @@ ActionMailer::Base.smtp_settings = {
     :domain               => "gmx.es",
     :user_name            => "eatinggoapp@gmx.es",
     :password             => "appEGO2017",
-    :authentication       => "plain",
+    :authentication       => "login",
     :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
     :enable_starttls_auto => true }
 
