6fee4a157c8185605ed925edc32086002139cd98
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-08-17 15:38:31 -0300

    prueba correo gmx fallida, vuelve gmail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -7,20 +7,13 @@ ActionMailer::Base.smtp_settings = {
    #  :authentication       => "plain",
    #  :enable_starttls_auto => true }
 
-    # :address              => "smtp.gmail.com",
-    # :port                 => 587,
-    # :domain               => "gmail.com",
-    # :user_name            => "eattinggo@gmail.com",
-    # :password             => "appEGO2017",
-    # :authentication       => "plain",
-
-    :address              => "mail.gmx.es",
+    :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmx.es",
-    :user_name            => "eatinggoapp@gmx.es",
+    :domain               => "gmail.com",
+    :user_name            => "eattinggo@gmail.com",
     :password             => "appEGO2017",
     :authentication       => "plain",
-    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+
     :enable_starttls_auto => true }
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
