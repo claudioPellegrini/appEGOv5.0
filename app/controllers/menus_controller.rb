c656ecadd43d6364cc093b4b46081fb2132d25f4
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-09-22 14:23:35 -0300

    try mails

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -134,8 +134,9 @@ class MenusController < ApplicationController
     end
     begin
       CuentaMailer.mailing(@destinatarios, @menu).deliver_now
-    rescue
-      format.html { redirect_to @menu, notice: 'Menú creado correctamente, pero ocurrio un error en el envio de los correos, por favor contacte a su proveedor SMTP .' }
+    rescue Exception => e
+      e.message
+      #format.html { redirect_to @menu, notice: 'Menú creado correctamente, pero ocurrio un error en el envio de los correos, por favor contacte a su proveedor SMTP .' }
     end
   end
 
